8af9cd33de9fa3950abd73d5cab6377208631981
Jose Ferreira <jpedroferreira95@gmail.com>
2025-11-12 17:54:47 +0000

    feat: add landing page with positioning

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,13 +1,30 @@
-import './App.css'
+import { ArrowUpIcon, BirdIcon } from "lucide-react";
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-
+  const alertMessage = () => {
+    return alert("Not Finished");
+  };
 
   return (
     <>
-  
+      <div className=" flex flex-col items-center space-y-10 m-[50%] lg:m-[20%]">
+        <BirdIcon
+          className="animate-bounce size-20"
+          onClick={alertMessage}
+        ></BirdIcon>
+        <ArrowUpIcon></ArrowUpIcon>
+        <Button
+          className=" hover:bg-blue-500"
+          variant={"outline"}
+          onClick={alertMessage}
+        >
+          Clica Aqui
+        </Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
